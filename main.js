b42d96682814131aa4f672964387970c6ecc525f
Nicole Rodriguez <nicolerodriguez@Nicoles-Air.lan>
2023-03-30 03:38:54 -0400

    hi

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-fetch(`https://random-stuff-api.p.rapidapi.com/joke/random`)
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+// fetch(`https://random-stuff-api.p.rapidapi.com/joke/random`)
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
