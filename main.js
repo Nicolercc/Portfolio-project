a3cd20ed68146968323c2707cae85f8259a2a267
Nicole Rodriguez <nicolerodriguez@Nicoles-Air.lan>
2023-04-02 18:06:06 -0400

    added forms

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// MAIN.JS
+
 const options = {
   method: "GET",
   headers: {
@@ -7,14 +9,6 @@ const options = {
   },
 };
 
-// fetch(
-//   "https://random-stuff-api.p.rapidapi.com/joke/random?exclude=dirty%2Cmoney",
-//   options
-// )
-//   .then((response) => response.json())
-//   .then((response) => console.log(response))
-//   .catch((err) => console.error(err));
-
 const jokeBtn = document.querySelector("#generate-joke-btn");
 const jokeParagraph = document.querySelector("#next-joke-text");
 const jokeScreen = document.querySelector("#joke-screen");
@@ -22,35 +16,66 @@ const homeScreen = document.querySelector("#home-screen-container");
 const jokeText = document.querySelector("#next-joke-text");
 // console.log(homeScreen);
 const nextJoke = document.querySelector("#another-joke-btn");
-jokeBtn.addEventListener("click", (event) => {
-  fetch(
-    "https://random-stuff-api.p.rapidapi.com/joke/puns?exclude=dirty%2Csex%2racist%2black",
-    options
-  )
-    .then((response) => response.json())
-    .then((response) => {
-      // jokeParagraph.classList.add("box");
-      jokeParagraph.innerText = response.message;
-      jokeScreen.classList.remove("hidden");
-      homeScreen.style.display = "none";
-    })
-    .catch((err) => console.error(err));
+
+const generateJokeForm = document.querySelector("#generate_joke_form");
+const generateAnotherJokeForm = document.querySelector("#another-joke-form");
+const submitErrorP = document.querySelector("#submit_error");
+
+generateJokeForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formDataEntries = new FormData(event.target).entries();
+  let areExternalRequestsAllowed = false;
+
+  for (let [inputName, inputValue] of formDataEntries) {
+    if (inputName === "ext_request_input" && inputValue === "on") {
+      areExternalRequestsAllowed = true;
+    }
+  }
+
+  if (areExternalRequestsAllowed) {
+    fetch(
+      "https://random-stuff-api.p.rapidapi.com/joke/puns?exclude=dirty%2Csex%2racist%2black",
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        jokeParagraph.innerText = response.message;
+        jokeScreen.classList.remove("hidden");
+        homeScreen.style.display = "none";
+      })
+      .catch((err) => console.error(err));
+  } else {
+    window.alert("YOU MUST ALLOW FOR EXTERNAL REQUESTS TO GET RANDOM JOKES");
+  }
 });
 
-nextJoke.addEventListener("click", (event) => {
+generateAnotherJokeForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  fetch(
-    "https://random-stuff-api.p.rapidapi.com/joke/puns?exclude=dirty%2Csex%2racist",
-    options
-  )
-    .then((response) => response.json())
-    .then((response) => {
-      //jokeText.innerText = response.message;
-      //jokeParagraph.classList.remove("hidden");
-      jokeText.innerText = response.message;
-      jokeParagraph.classList.add("hidden");
-      jokeScreen.style.display = "block";
-      homeScreen.style.display = "none";
-    })
-    .catch((err) => console.error(err));
+
+  const formDataEntries = new FormData(event.target).entries();
+  let areExternalRequestsAllowed = false;
+
+  for (let [inputName, inputValue] of formDataEntries) {
+    if (inputName === "ext_request_input" && inputValue === "on") {
+      areExternalRequestsAllowed = true;
+    }
+  }
+
+  if (areExternalRequestsAllowed) {
+    fetch(
+      "https://random-stuff-api.p.rapidapi.com/joke/puns?exclude=dirty%2Csex%2racist",
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        jokeText.innerText = response.message;
+        jokeParagraph.classList.add("hidden");
+        jokeScreen.style.display = "block";
+        homeScreen.style.display = "none";
+      })
+      .catch((err) => console.error(err));
+  } else {
+    window.alert("YOU MUST ALLOW FOR EXTERNAL REQUESTS TO GET RANDOM JOKES");
+  }
 });
